b525708a4fb44fd88524da381af25bd93134d7f4
harshjivani1811 <85457134+harshjivani1811@users.noreply.github.com>
2021-06-07 00:55:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript & React.js</h1>
+      <p> This is my first project. I have learn lot of things with help of ShapeAI. This is the best experience eith Javascript. Thank You ShapeAI.</p>
+    </div>
+  );
+}
+
+export default Note;
